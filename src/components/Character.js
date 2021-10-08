4e71c51b7a27d1aa51f2879cef395849fb559bf6
Hannah Brog <bugsbrog@Hannahs-MBP.home>
2021-10-08 09:54:12 -0600

    first push

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,7 @@
 // Write your Character component here
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Characters = (props) => {
+    const {characters} = props;
+}
