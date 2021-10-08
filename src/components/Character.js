b5218be1f31a16788a0142eb6b6be47c19497471
Hannah Brog <bugsbrog@Hannahs-MBP.home>
2021-10-08 10:39:35 -0600

    All tests passing!

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,23 @@
 // Write your Character component here
 import React, { useState } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 const Characters = (props) => {
     const {characters} = props;
-}
+
+    return (
+        <div>
+        {characters.map(res => {
+            return ( 
+            <div> 
+                <h1>{res.name}</h1>
+                <h2>{res.birth_year}</h2>
+                <h2>{res.gender}</h2>
+            </div>
+            )
+    })}
+    </div>
+        )
+    }
+
+export default Characters
